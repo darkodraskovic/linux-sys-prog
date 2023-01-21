31b3d115adf354609fbcc2830aa683d27f43459f
Darko Draskovic <darko.draskovic@gmail.com>
2023-01-21 13:24:07 +0100

    Add read() and write() examples
    Signed-off-by: Darko Draskovic <darko.draskovic@gmail.com>

--- src/02_File_IO/main.c
+++ src/02_File_IO/main.c
@@ -1,16 +1,43 @@
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char** argv) {
-   int fd;
+  int fd_creat = creat("../assets/creat.txt", 0644);
+  if (fd_creat == -1) {
+  }
 
-   fd = open("../assets/raven.txt", O_RDONLY);
+  int fd_raven = open("../assets/raven.txt", O_RDONLY);
+  if (fd_raven == -1) {
+  }
 
-   if (fd == -1) {
+  //   unsigned long word;
+  //   ssize_t nr = read(fd, &word, sizeof(unsigned long));
 
-   }
+  ssize_t ret;
+  ssize_t len = 1024 * 1024;
+  char* buf = malloc(len * sizeof(char));
+  char* buf_pos = buf;
+  while (len != 0 && (ret = read(fd_raven, buf_pos, len)) != 0) {
+    if (ret == -1) {
+      if (errno == EINTR) continue;
+      perror("read");
+      break;
+    }
 
-   return 0;
+    len -= ret;
+    buf_pos += ret;
+  }
+
+  //   printf("%s\n", buf);
+
+  const char* buf2 = "My ship is solid!";
+  ssize_t nr = write(fd_creat, buf2, strlen(buf2));
+
+  return 0;
 }
